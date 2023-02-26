16692687c4cbbd84d11577f18da7fb9a14e45065
Gavin Fortner <gavin.fortner.8@gmail.com>
2023-02-25 23:41:41 -0500

    sprint 3 bare bones requirements

--- login_app/urls.py
+++ login_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login_dashboard, name="login_dashboard"),
+    path('student', views.student_homepage, name='student_homepage'),
+    path('advisor', views.advisor_homepage, name='advisor_homepage'),
 ]
--- login_app/views.py
+++ login_app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def login_dashboard(request):
     return render(request, 'main.html', {})
+
+def student_homepage(request):
+    return render(request, 'student_homepage.html', )
+
+
+def advisor_homepage(request):
+    return render(request, 'advisor_homepage.html', )
