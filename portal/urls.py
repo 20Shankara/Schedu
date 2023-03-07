1c525ae8f41d7df618411d474f4e16e283e043bf
Gavin Fortner <gavin.fortner.8@gmail.com>
2023-03-07 17:57:04 -0500

    Student user type Complete

--- portal/urls.py
+++ portal/urls.py
@@ -6,5 +6,5 @@ app_name = 'portal'
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('studentSignUp', views.student_sign_up, name='student_link'),
-    path('studentDashboard', views.student_dashboard, name='student_dashboard'),
+    path('<int:student_id>/student_dashboard/', views.student_dashboard, name='student_dashboard'),
 ]
--- portal/views.py
+++ portal/views.py
@@ -10,41 +10,22 @@ def homepage(request):
     # from https://stackoverflow.com/questions/14639106/how-can-i-retrieve-a-list-of-field-for-all-objects-in-django 
     uva_students = list(Student.objects.all().values_list('student_email', flat=True))
     uva_advisors = list(Advisor.objects.all().values_list('advisor_email', flat=True))
-    print(uva_students)
-    print(uva_advisors)
-    return render(request, 'pages/home.html', {"students": uva_students, "advisors": uva_advisors})
+    print(request.user)
+    if request.user.is_authenticated:
+        print("LOGGED IN")
+        if request.user.email in uva_students:
+            print("FOUND A STUDENT")
+            student = Student.objects.get(student_email=request.user.email)
+            print(student.id)
+            return render(request, 'pages/student_dashboard.html', {"student": student})
+        if request.user.email in uva_advisors:
+            print("found an advisor")
+            # eventually, mke this go to advisor dashboard
+            return render(request, 'pages/home.html', {"students": uva_students, "advisors": uva_advisors})
+    else:
+        print("LOG IN WITH GOOGLE or SIGN UP")
+        return render(request, 'pages/home.html', {"students": uva_students, "advisors": uva_advisors})
 
-
-# def student_sign_up(request):
-#     if request.method == "POST":
-#         form = StudentForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             print("-------")
-#             fn = form.cleaned_data.get('student_first_name')
-#             ln = form.cleaned_data.get('student_last_name')
-#             yr = form.cleaned_data.get('year_in_school')
-#             fn2 = request.POST['student_first_name']
-#             ln2 = request.POST['student_last_name']
-#             # newStudent = Student(student_first_name=fn2+'zzz', student_last_name=ln2+'zzz')
-#             # newStudent.save()
-#             print("-------")
-#             return HttpResponseRedirect(reverse('portal:student_dashboard', {"form": form}))
-#     else:
-#         form = StudentForm()
-#
-#     return render(request, "pages/student_sign_up.html")
-
-# def student_sign_up(request):
-#     if request.method == "POST":
-#         form = StudentForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('portal:student_dashboard'))
-#     else:
-#         form = StudentForm()
-#
-#     return render(request, "pages/student_sign_up.html", {"form": form})
 
 def student_sign_up(request):
     if request.method == "POST":
@@ -59,15 +40,17 @@ def student_sign_up(request):
         print(year)
         print('----------')
         # add some logic here for year
-        newStudent = Student(student_first_name=first_name, student_last_name=last_name, student_email=email, year_in_school=year)
+        newStudent = Student(student_first_name=first_name, student_last_name=last_name, student_email=email,
+                             year_in_school=year)
         newStudent.save()
-        # this should be redirect...in dashboard can fetch all information by getting student profile based off email
-        return render(request, 'pages/student_dashboard.html')
+        print(newStudent.id)
+        return HttpResponseRedirect(reverse('portal:student_dashboard', args=(newStudent.id,)))
 
     return render(request, "pages/student_sign_up.html")
 
 
-def student_dashboard(request):
-    students = Student.objects.all()  # fetch by email here, instead
-    print(students)
-    return render(request, 'pages/student_dashboard.html', {"Students": students})
+def student_dashboard(request, student_id):
+    student = Student.objects.get(pk=student_id)
+    print(student)
+    print("HIIIIIII")
+    return render(request, 'pages/student_dashboard.html', {"student": student})
