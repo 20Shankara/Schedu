9a8f008337a996395a0ccff650797535f48a3ed5
Ananya-Ananda <ananya.ananda201@gmail.com>
2023-03-15 12:20:52 -0400

    course number search

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,6 +80,20 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #     "NAME": "d91j10444ek3fv",
+    #     "USER": "thchwasvixrbtr",
+    #     "PASSWORD": "20396eed5d08e0c6e76897decc963b34bb4f13fddf0df62320a8273499474e6e",
+    #     "HOST": "ec2-3-209-124-113.compute-1.amazonaws.com",
+    #     "PORT": "5432",
+    #     "DATABASE_URI": "postgres://thchwasvixrbtr:20396eed5d08e0c6e76897decc963b34bb4f13fddf0df62320a8273499474e6e@ec2-3-209-124-113.compute-1.amazonaws.com:5432/d91j10444ek3fv",
+    #     "HEROKU_CLI": "heroku pg:psql postgresql-fitted-32662 --app safe-lake-70180",
+    # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "d91j10444ek3fv",
@@ -90,10 +104,6 @@ DATABASES = {
         "DATABASE_URI": "postgres://thchwasvixrbtr:20396eed5d08e0c6e76897decc963b34bb4f13fddf0df62320a8273499474e6e@ec2-3-209-124-113.compute-1.amazonaws.com:5432/d91j10444ek3fv",
         "HEROKU_CLI": "heroku pg:psql postgresql-fitted-32662 --app safe-lake-70180",
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
 
 # Password validation
