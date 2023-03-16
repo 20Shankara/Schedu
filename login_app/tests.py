8e404754340672aa17cb0847e8cd7eb66bdd865c
Ashwin <as7nj@virginia.edu>
2023-03-15 21:38:27 -0400

    testStatus

--- login_app/tests.py
+++ login_app/tests.py
@@ -4,4 +4,14 @@ class StatusCodeTests(TestCase):
     def test_login_page_status(self):
         client = Client()
         response = client.get('/')
-        self.assertIs(response.status_code, "200")
+        self.assertEqual(response.status_code, 200)
+
+    def test_advisor_sign_up(self):
+        client = Client()
+        response = client.get('/advisorSignUp')
+        self.assertEqual(response.status_code, 200)
+
+    def test_student_sign_up(self):
+        client = Client()
+        response = client.get('/studentSignUp')
+        self.assertEqual(response.status_code, 200)
