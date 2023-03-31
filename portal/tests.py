fc2e64467cd836fe309a5217f5644db97fcf57ea
Gavin Fortner <gavin.fortner.8@gmail.com>
2023-03-30 22:05:15 -0400

    edited tests.py path

--- portal/tests.py
+++ portal/tests.py
@@ -3,10 +3,10 @@ from django.test import TestCase, Client
 class status_code_tests(TestCase):
     def test_advisor(self):
         client = Client()
-        response = client.get('/advisor')
+        response = client.get('/advisor_sign_up')
         self.assertEqual(response.status_code, 200)
 
     def test_student(self):
         client = Client()
-        response = client.get('/student')
+        response = client.get('/student_sign_up')
         self.assertEqual(response.status_code, 200)
