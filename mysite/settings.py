0f6f89b2fb6d6f70d643804a89f2a7d7cc3c32f9
20Shankara <101889969+20Shankara@users.noreply.github.com>
2023-05-18 21:41:03 -0400

    init

--- mysite/settings.py
+++ mysite/settings.py
@@ -181,5 +181,13 @@ try:
         django_heroku.settings(locals())
 except ImportError:
     found = False
-    
-SECURE_SSL_REDIRECT = True
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
